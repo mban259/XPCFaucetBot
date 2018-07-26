77fcdbdb977e19ceb48bfb3297743f1abdb5e914
mban <mban259@users.noreply.github.com>
2018-07-27 01:39:14 +0900

    コマンドをDMで入力した際の変更

--- XPCFaucetBot/Events/Messages/MessageMonitor.cs
+++ XPCFaucetBot/Events/Messages/MessageMonitor.cs
@@ -51,6 +51,8 @@ namespace XPCFaucetBot.Events.Messages
                 {
                     Debug.Log(result.ErrorReason);
                 }
+
+                return;
             }
 
             if (context.IsPrivate)
